48f79273418cbc0741e758a28a264af179b25549
kba1986cr <haroldsa1986@hotmail.com>
2024-12-13 08:20:10 -0600

    Esta

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 // Grupo de rutas protegidas por el middleware de autenticación
-Route::middleware('auth')->group(function () {
+// Route::middleware('auth')->group(function () {
     // Ruta para crear el CV
     Route::get('/cv/create', [CvController::class, 'create'])->name('cv.create');
 
@@ -32,7 +32,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// });
 
 // Inclusión de rutas de autenticación (login, registro, etc.)
 require __DIR__ . '/auth.php';
